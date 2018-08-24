e61701274ca24f0aac4bf7215f077dcfdb317fee
4ugust0 <jass2@ic.ufal.br>
2018-08-23 23:20:58 -0300

    Third commit

--- src/iSoccer/Persons/Employees/Team_Workers/Cooker.java
+++
@@ -1,9 +0,0 @@
-package iSoccer.Persons.Employees.Team_Workers;
-
-import iSoccer.Persons.Employees.Employee;
-
-public class Cooker extends Employee {
-	public Cooker(String fullName, String eMail, long cpfNumber, long phoneNumber, double monthlySalary) {
-		super(fullName, eMail, cpfNumber, phoneNumber, monthlySalary);
-	}
-}
--- src/iSoccer/Persons/Employees/Team_Workers/Lawyer.java
+++
@@ -1,9 +0,0 @@
-package iSoccer.Persons.Employees.Team_Workers;
-
-import iSoccer.Persons.Employees.Employee;
-
-public class Lawyer extends Employee {
-	public Lawyer(String fullName, String eMail, long cpfNumber, long phoneNumber, double monthlySalary) {
-		super(fullName, eMail, cpfNumber, phoneNumber, monthlySalary);
-	}
-}
